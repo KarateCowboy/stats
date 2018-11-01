ddc7691521cb92fee3d1040b8608f82d4ea75052
Matthew Origer <moriger@brave.com>
2018-11-01 12:38:16 -0500

    works for getting prod data

--- bin/get_logs.js
+++ bin/get_logs.js
@@ -1,7 +1,6 @@
 const AWS = require('aws-sdk')
 AWS.config.setPromisesDependency(null)
 const Knex = require('knex')
-const FS = require('fs-extra')
 const request = require('request')
 const feathers = require('@feathersjs/feathers')
 const auth = require('@feathersjs/authentication-client')
@@ -10,20 +9,11 @@ const _ = require('underscore')
 const commander = require('commander')
 const moment = require('moment')
 let numbers_host, numbers_port, numbers_user, numbers_pwd, numbers_app
-commander.option('-d --day [num]', 'Days to go back', 90)
-
-let latest_key_for_ymd = async function (ymd) {
-  const latest_key = knex('dw.downloads').whereRaw(`key LIKE '%${ymd}%' `).orderBy('created_at', 'desc').limit(1).toString()
-  const result = await knex.raw(latest_key)
-  if (result.rows.length === 1) {
-    return result.rows[0].key
-  } else {
-    return undefined
-  }
-}
+let DownloadsService = require('../src/services/downloads.service')
+commander.option('-d --day [string]', 'Days to go back', moment().subtract(1, 'days').format('YYYY-MM-DD')).parse(process.argv)
 
 const main = async (day) => {
-  const dayPrefix = moment().subtract(day, 'days').format('YYYY-MM-DD')
+  const dayPrefix = day
   await validate_env()
   numbers_app = feathers()
   const rest_client = Rest('http://' + numbers_host + ':' + numbers_port)
@@ -32,53 +22,9 @@ const main = async (day) => {
   await connect()
   const S3 = new AWS.S3({})
   const download_logs_bucket = 'brave-download-logs'
-  const params = {
-    Bucket: download_logs_bucket,
-    MaxKeys: 2000000000,
-    Prefix: dayPrefix,
-  }
-
-  let latest_key = await latest_key_for_ymd(dayPrefix)
-  if (latest_key) {
-    params.Marker = latest_key
-  }
-
-  // Retrieve list of log files, parse them and return records as
-  // an array of objects
-  let objects = []
-  let toLoad = []
-  console.log('getting data')
-  let data = await S3.listObjects(params).promise() //, data, (err, data) => { if (err) return reject() resolve(data.Contents)
-  let count = 0
-  while (data.Contents.length > 0 && objects.length < 50000000) {
-    objects = objects.concat(data.Contents)
-    toLoad = toLoad.concat(data.Contents)
-    params.Marker = _.last(data.Contents).Key
-    data = await S3.listObjects(params).promise()
-    if (toLoad.length > 1000) {
-      await Promise.all(toLoad.map(async (file) => {
-        let object
-        try {
-          const exists = await knex('dw.downloads').where({key: file.Key}).count()
-          if (Number(exists[0].count) === 0) {
-            object = await  S3.getObject({Bucket: 'brave-download-logs', Key: file.Key}).promise()
-            let str = object.Body.toString()
-            await numbers_app.service('downloads').create({rawString: str, key: file.Key})
-          }
-        } catch (e) {
-          if (!!!e.message.match(/duplicate/)) {
-            // console.log(`error on ${object.Body.toString()}`)
-            console.log(`Problem: ${e.message}`)
-          }
-        }
-        count++
-        if (count % 10000 === 0) {
-          console.log(count)
-        }
-      }))
-      toLoad = []
-    }
-  }
+  const downloadsService = new DownloadsService(download_logs_bucket, S3, dayPrefix, numbers_app)
+  await downloadsService.getObjectListForDay()
+  await downloadsService.loadObjectsFromList()
   console.log(`done loading day ${dayPrefix}`)
 }
 
@@ -120,7 +66,6 @@ const connect = async function () {
     })
     console.log(`Connected to Numbers at ${numbers_host}:${numbers_port}`)
   } catch (e) {
-    console.log('stuff')
     console.log(e.message)
     process.exit()
   }
---
+++ src/services/downloads.service.js
@@ -0,0 +1,52 @@
+const _ = require('underscore')
+const ProgressBar = require('smooth-progress')
+module.exports = class DownloadsService {
+  constructor (downloads_bucket, S3, ymd, numbersApp) {
+    this.download_logs_bucket = downloads_bucket
+    this.S3 = S3
+    this.objectList = []
+    this.ymd = ymd
+    this.numbersApp = numbersApp
+  }
+
+  async getObjectListForDay () {
+    const params = {
+      Bucket: this.download_logs_bucket,
+      MaxKeys: 2000000000,
+      Prefix: this.ymd,
+    }
+    let data = await this.S3.listObjects(params).promise()
+    while (data.Contents.length > 0) {
+      this.objectList = this.objectList.concat(data.Contents)
+      params.Marker = _.last(data.Contents).Key
+      data = await this.S3.listObjects(params).promise()
+    }
+  }
+
+  async loadObjectsFromList () {
+    const bar = ProgressBar({
+      tmpl: `Loading ${this.objectList.length} ... :bar :percent :eta`,
+      width: 25,
+      total: this.objectList.length
+    })
+    for (let object of this.objectList) {
+      const exists = await knex('dw.downloads').where({key: object.Key}).count()
+      if (Number(exists[0].count) === 0) {
+        object = await  this.S3.getObject({Bucket: 'brave-download-logs', Key: object.Key}).promise()
+        let str = object.Body.toString()
+        await this.numbersApp.service('downloads').create({rawString: str, key: object.Key})
+      }
+      bar.tick(1)
+    }
+  }
+
+  async latestKeyForYmd (ymd) {
+    const latest_key = knex('dw.downloads').whereRaw(`key LIKE '%${ymd}%' `).orderBy('created_at', 'desc').limit(1).toString()
+    const result = await knex.raw(latest_key)
+    if (result.rows.length === 1) {
+      return result.rows[0].key
+    } else {
+      return undefined
+    }
+  }
+}
