6a7068f8fb347f124270da44d9738b675ca2ba3b
Matthew Origer <karatecowboy@gmail.com>
2018-12-17 14:55:06 -0600

    update downloads getting script
    needs to use the new database connection

--- bin/get_logs.js
+++ bin/get_logs.js
@@ -1,16 +1,16 @@
 const AWS = require('aws-sdk')
 AWS.config.setPromisesDependency(null)
-const Knex = require('knex')
 const mongoose = require('mongoose')
 const _ = require('underscore')
 const commander = require('commander')
 const Sequelize = require('sequelize')
+const DbUtil = require('../src/models')
 const moment = require('moment')
 let DownloadsService = require('../src/services/downloads.service')
 commander.option('-d --day [string]', 'Days to go back', moment().subtract(1, 'days').format('YYYY-MM-DD')).parse(process.argv)
 
 const main = async (day) => {
-  const dayPrefix = moment().subtract(day,'days').format('YYYY-MM-DD')
+  const dayPrefix = moment().subtract(day, 'days').format('YYYY-MM-DD')
   await connect()
   const S3 = new AWS.S3({})
   const download_logs_bucket = 'brave-download-logs'
@@ -22,7 +22,7 @@ const main = async (day) => {
 }
 
 const connect = async function () {
-  global.sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false })
+  global.sequelize = new Sequelize(process.env.DATABASE_URL, {logging: false})
   AWS.config.update({
     accessKeyId: process.env.AWS_KEY,
     secretAccessKey: process.env.AWS_SECRET,
@@ -30,6 +30,7 @@ const connect = async function () {
     sslEnabled: true
   })
   await mongoose.connect(process.env.MLAB_URI)
+  global.db = new DbUtil(process.env.DATABASE_URL)
   console.log('configured AWS')
 }
 
