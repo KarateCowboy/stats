7791bbdfa9f41e5b3746c8a4e0a8709b7a65a5af
Matthew Origer <karatecowboy@gmail.com>
2019-06-06 12:59:35 -0500

    Add KYC and Uphold to publishers data
    Github issue https://github.com/brave/stats/issues/313
    
    Add kyc and uphold verified numbers to aggregates
    
    UI Changes:
    
    - new metric on Daily Publishers report
    - new metric on Daily Publishers Aggregated report
    
    Tests:
    
    - kyc uphold verified attribute on PublisherSignupDay model
    - PublisherSignupDay `buildFromRemote` hits new endpoint
    - added attribute to fixture file
    - adjusted api tests to consider added results
    
    Migrations:
    
    - add column to `dw.pubisher_signup_days` table
    
    API & Model
    
    - adjust `buildFromRemote`, `asYmd`, and model validator to use new attribute and data
    - add kyc uphold to aggregate endpoint for daily publishers

--- src/models/validators/publisher_signup_day.js
+++ src/models/validators/publisher_signup_day.js
@@ -5,6 +5,7 @@ const PublisherSignupDaySchema = Joi.object().keys({
   'email_channel_and_uphold_verified': Joi.number().integer(),
   'email_channel_verified': Joi.number().integer(),
   'email_verified': Joi.number().integer(),
+  'kyc_uphold_and_email_verified': Joi.number().integer(),
   'ymd': Joi.date(),
   'created_at': Joi.date(),
   'updated_at': Joi.date()
--- test/test_helper.js
+++ test/test_helper.js
@@ -46,7 +46,6 @@ class TestHelper {
     }
     this.testDatabaseUrl = process.env.TEST_DATABASE_URL
     global.SQL_ORM_URL = process.env.TEST_DATABASE_URL
-    global.sequelize = new Sequelize(SQL_ORM_URL, {logging: false})
     if (!process.env.TEST_MLAB_URI) {
       throw Error('Please set TEST_MLAB_URI')
     }
@@ -118,7 +117,7 @@ class TestHelper {
     }
     if (!global.pg_client) {
       global.pg_client = await pg.connect(this.testDatabaseUrl)
-      this.knex = await Knex({client: 'pg', connection: this.testDatabaseUrl})
+      this.knex = await Knex({ client: 'pg', connection: this.testDatabaseUrl })
       global.knex = this.knex
     }
     if (global.db === undefined) {
