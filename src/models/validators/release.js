abd7d307427acc0ac79930d49798f639728fcd79
Matthew Origer <moriger@brave.com>
2019-08-13 11:50:03 -0500

    Crash Ratios Report Fix & Update
    Github issue [347](https://github.com/brave/stats/issues/347)
    
    Make crash ratios report work with brave core builds and crashes
    
    CLI Changes:
    
    - new script `build-releases-from-crashes`
    
    UI Changes:
    
    - update internals of Crash Ratios report to use new API data
    
    Migrations:
    
    - 20190611145805_update-canonical-platforms.js
    - 20190715163000_add-channel-to-crashes-dau-mv.js
    - 20190726125107_create-releases.js
    - 20190726132248_populate-releases.js
    - 20190729002942_create-release-crashes.js
    - 20190730143334_add-release-data-to-crashes-dau-mv.js
    - 20190806131234_canonical-platforms-use-bc.js
    - 20190808105935_readd-channel-to-crashes-dau-mv.js
    
    API & Model
    
    - API
       - Releases `POST` and `GET` methods
       - add `chromium_version` to `crash_ratios` endpoint
    - Model
       - crash fixture for crash model
       - Releases model
       - ReleaseCrashes model and validators
       - add Releases relation to UsageSummary model
    
    Tests:
    
    - crash models and API
    - release models and API - usagesummary model
    - crashes_dau_mv material view

--- src/models/validators/release.js
+++ src/models/validators/release.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
-const PlatformSchema = Joi.object().keys({
+const ReleaseSchema = Joi.object().keys({
   chromium_version: Joi.string().required(),
   brave_version: Joi.string().required(),
   uses_hybrid_format: Joi.boolean()
 })
 
-module.exports = PlatformSchema
+module.exports = ReleaseSchema
