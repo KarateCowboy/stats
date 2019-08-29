dc557c71b039e98f7487851d801b62791b3093b7
Aubrey Keus <aekeus@gmail.com>
2019-08-29 18:38:44 -0400

    Use cached result for remote jobs

--- src/api/retention.js
+++ src/api/retention.js
@@ -1,42 +1,15 @@
-const common = require('./common')
-const moment = require('moment')
-const _ = require('underscore')
 const remote = require('../remote-job')
 
 exports.setup = async (server, client, mongo, ch) => {
   server.route({
     method: 'GET',
     path: '/api/1/retention_cc',
-    handler: async (request, h) => {
-      try {
-        let jobId = await remote.initialize(
-          client,
-          ch,
-          'country-retention',
-          common.retrieveCommonParametersObject(request)
-        )
-        return { id: jobId }
-      } catch (e) {
-        console.log(e)
-      }
-    }
+    handler: remote.jobHandler(client, ch, 'country-retention')
   })
 
   server.route({
     method: 'GET',
     path: '/api/1/retention_week',
-    handler: async (request, h) => {
-      try {
-        let jobId = await remote.initialize(
-          client,
-          ch,
-          'weekly-retention',
-          common.retrieveCommonParametersObject(request)
-        )
-        return { id: jobId }
-      } catch (e) {
-        console.log(e)
-      }
-    }
+    handler: remote.jobHandler(client, ch, 'weekly-retention')
   })
 }
